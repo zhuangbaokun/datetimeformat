2491f0df7fc764d236260f4f41e0f3851c6275c8
zhuangbaokun <zhuangbaokun@gmail.com>
2021-05-13 10:20:48 +0800

    udpate setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'datetimeformat',
     packages = ['datetimeformat'],
-    version = 'v1.0',
+    version = 'v2.0',
     description = 'Infer datetime format from datetime string or standardize datetime and timezone format',
     author = 'Zhuang Baokun',
     author_email = 'zhuangbaokun@gmail.com',
     url = 'https://github.com/zhuangbaokun/datetimeformat',
-    download_url = 'https://github.com/zhuangbaokun/datetimeformat/archive/refs/tags/v1.0.tar.gz',
+    download_url = 'https://github.com/zhuangbaokun/datetimeformat/archive/refs/tags/v2.0.tar.gz',
     keywords = ['datetime', 'format','detect','infer','standardize datetime and timezone format'],
     classifiers = [],
 )
